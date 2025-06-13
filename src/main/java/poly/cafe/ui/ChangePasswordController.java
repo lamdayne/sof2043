996f27b297a2c7cae9cee75dce4bfb745105963f
lamdayne <nlam32428@gmail.com>
2025-06-13 19:39:00 +0700

    Add folder ui

---
+++ src/main/java/poly/cafe/ui/ChangePasswordController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package poly.cafe.ui;
+
+/**
+ *
+ * @author PHUONG LAM
+ */
+public interface ChangePasswordController {
+    void open();
+    void save();
+    void close();
+}
