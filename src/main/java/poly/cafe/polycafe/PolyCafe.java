edb18f82b5f9ca0f625d35ad1f7a7754908ab257
lamdayne <nlam32428@gmail.com>
2025-06-13 19:41:21 +0700

    PolyCafe

---
+++ src/main/java/poly/cafe/polycafe/PolyCafe.java
@@ -0,0 +1,12 @@
+package poly.cafe.polycafe;
+
+import poly.cafe.ui.PolyCafeJFrame;
+
+public class PolyCafe {
+
+    public static void main(String[] args) {
+        PolyCafeJFrame polycafe = new PolyCafeJFrame();
+        polycafe.show();
+        
+    }
+}
