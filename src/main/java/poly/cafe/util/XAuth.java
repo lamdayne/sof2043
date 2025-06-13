5fce6fd5a980e1ef47255d8053e2aacafd6a654c
lamdayne <nlam32428@gmail.com>
2025-06-13 19:39:39 +0700

    Add folder util

---
+++ src/main/java/poly/cafe/util/XAuth.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.cafe.util;
+
+import poly.cafe.entity.User;
+
+/**
+ *
+ * @author PHUONG LAM
+ */
+public class XAuth {
+
+    public static User user = User.builder()
+            .username("user1@gmail.com")
+            .password("123")
+            .enabled(true)
+            .manager(true)
+            .fullname("Nguyễn Văn Tèo")
+            .photo("trump.png")
+            .build();
+    
+}
